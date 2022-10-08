f47a7646b133eb5293cfbf53a0d559f5a0116390
Agustín Ruatta <agustinruatta@gmail.com>
2022-10-08 18:58:55 -0300

    Fix error

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DappToken is ERC20, Ownable {
-    constructor() ERC20("LPToken", "LPT") {}
+    constructor() ERC20("DappToken", "DAPP") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
