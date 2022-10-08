210832aac9981a7cd5c41aee0d7f4a08529a18f9
Agustín Ruatta <agustinruatta@gmail.com>
2022-10-08 18:52:24 -0300

    Create Dapp Token

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL v3
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DappToken is ERC20, Ownable {
+    constructor() ERC20("LPToken", "LPT") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
